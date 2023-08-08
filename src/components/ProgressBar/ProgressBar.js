286f3573537fb034872cff312e67d15f1f81ab14
Gabriel Galvez <ggalvez@konradgroup.com>
2023-08-08 13:38:05 -0600

    Implementation of ProgressBar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,65 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const PROGRESS_BAR_MIN_VALUE = 0;
+const PROGRESS_BAR_MAX_VALUE = 100;
+
+const SIZES = {
+  small: {
+    height: "8px",
+    borderRadius: "4px"
+  },
+  medium: {
+    height: "12px",
+    borderRadius: "4px"
+  },
+  large: {
+    height: "24px",
+    padding: "4px",
+    borderRadius: "8px"
+  }
+}
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const progressBarSize = SIZES[size];
+
+  return (
+    <Wrapper
+      style={progressBarSize}
+      role="progressbar"
+      aria-label="progress bar"
+      aria-valuenow={value}
+      aria-valuemin={PROGRESS_BAR_MIN_VALUE}
+      aria-valuemax={PROGRESS_BAR_MAX_VALUE}
+    >
+      <BarTrimmer>
+        <Bar value={value}>
+          <VisuallyHidden>
+            {value}%
+          </VisuallyHidden>
+        </Bar>
+      </BarTrimmer>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  background-color: ${COLORS.transparentGray15};
+  width: 100%;
+`;
+
+const BarTrimmer = styled.div`
+  overflow: hidden;
+  border-radius: 4px;
+  width: 100%;
+  height: 100%;
+`;
+
+const Bar = styled.div`
+  background-color: ${COLORS.primary};
+  width: ${props => props.value + "%"};
+  height: 100%;
+`;
 
 export default ProgressBar;
