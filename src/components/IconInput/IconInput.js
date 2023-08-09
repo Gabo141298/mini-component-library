cc29b5f3218e1d8f61d27bcda0514d1d845ff072
Gabriel Galvez <ggalvez@konradgroup.com>
2023-08-09 16:44:32 -0600

    Improvements to IconInput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -9,16 +9,18 @@ import VisuallyHidden from '../VisuallyHidden';
 const SIZES = {
   small: {
     "--font-size": 14 / 16 + "rem",
-    "--border-weight": "1px",
+    "--border-thickness": "1px",
     "--padding-bottom": "4px",
     "--icon-text-separation": "24px",
+    "--height": 24 / 16 + "rem",
     iconSize: 16
   },
   large: {
     "--font-size": 18 / 16 + "rem",
-    "--border-weight": "2px",
+    "--border-thickness": "2px",
     "--padding-bottom": "7px",
     "--icon-text-separation": "36px",
+    "--height": 36 / 16 + "rem",
     iconSize: 24
   }
 }
@@ -28,27 +30,24 @@ const IconInput = ({
   icon,
   width = 250,
   size,
-  placeholder,
+  ...delegated
 }) => {
   const styles = SIZES[size];
 
   return (
     <Wrapper>
       <VisuallyHidden>{label}</VisuallyHidden>
-      <SearchWrapper>
-        <IconWrapper styles={{ "--size": styles.iconSize }}>
-          <Icon id={icon} size={styles.iconSize} />
-        </IconWrapper>
-        <Input placeholder={placeholder} style={{ ...styles, "--width": width + "px" }} />
-      </SearchWrapper>
+      <IconWrapper style={{ "--size": styles.iconSize + "px" }}>
+        <Icon id={icon} size={styles.iconSize} />
+      </IconWrapper>
+      <Input {...delegated} style={{ ...styles, "--width": width + "px" }} />
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div`
+const Wrapper = styled.label`
+  display: block;
   position: relative;
-  width: fit-content;
-  padding: 2px;
 
   color: ${COLORS.gray700};
 
@@ -57,35 +56,30 @@ const Wrapper = styled.div`
   }
 `;
 
-const SearchWrapper = styled.div`
-  position: relative;
-`;
-
 const IconWrapper = styled.div`
   position: absolute;
   top: 0;
-  left: 0;
   bottom: 0;
-  margin: auto;
+  left: 0;
+  margin: auto 0;
   height: var(--size);
   width: var(--size);
 `;
 
 const Input = styled.input`
   width: var(--width);
+  height: var(--height);
+
   font-size: var(--font-size);
   border: none;
-  border-bottom: var(--border-weight) solid ${COLORS.black};
+  border-bottom: var(--border-thickness) solid ${COLORS.black};
   
   padding-left: var(--icon-text-separation);
-  padding-bottom: var(--padding-bottom);
 
-  color: ${COLORS.gray700};
+  outline-offset: 2px;
+
+  color: inherit;
   font-weight: 700;
-
-  &:hover {
-    color: ${COLORS.black};
-  }
 
   &::placeholder {
     color: ${COLORS.gray500};
